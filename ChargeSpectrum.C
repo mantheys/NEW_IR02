a7792c579288d49bdb924550888fbc9d14bdf507
Sergio Manthey <manthey@pcae182.ciemat.es>
2022-02-28 16:42:42 +0100

    NEW_MACRO CHARGE SPECTRUM

--- ChargeSpectrum.C
+++ ChargeSpectrum.C
@@ -1,136 +1,259 @@
-/*
-Macro para obtener el espectro de carga de por canal y run.
-Usa como input las ntuplas generadas con la macro SimpleAnalysis.C
-*/
+// ---------------------------------- ChargeSpectrum ---------------------------------- //
+
+/* Macro para obtener el espectro de carga de por canal y run.
+   Usa como input las ntuplas generadas con la macro SimpleAnalysis.C
+   source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.18.04/x86_64-centos7-gcc48-opt/bin/thisroot.sh */
 
 #include"lib/headers.h"
 
-void Draw(std::vector<int> mych, std::vector<int> runs, std::vector<string> name, bool rate, std::vector<string> var, TVirtualPad *pad=NULL, float CustomMaxRange=0, double Rebin=2 )
+/* Draw_vector() -> Funcion con parametros vectorizados para todos los canales de manera que podemos representar todas las 
+cargas/amplitudes de todos los dias en el mismo histograma. Ademas se puede hacer un fit a todos y guardar los valores en un txt :)
+Se podria introducir una ganancia diferente para cada run aunque es más coherente usar un valor medio para todos los dias. */
+void Draw_vector(string path, std::vector<int> mych, std::vector<int> runs, std::vector<string> name, bool rate, std::vector<string> var, std::vector<double> conv_fact,
+double aef_sipm, double aef_pmt, double aef_sc, bool normalize=false, double Rebin=1, TVirtualPad *pad=NULL, float CustomMaxRange=0)
 {
+  std::vector<double> SPEAmp={38.6,24.8,25.5};//Amplitud del SPE en cuentas de ADC //de Rodrigo --> cambiar por nuevas
   int n = runs.size(); 
-  std::vector<double> Gains={7.90191,3.61,3.83}; //ganancias en pC
-  std::vector<double> SPEAmp={38.6,24.8,25.5};//Amplitud del SPE en cuentas de ADC
   std::vector<variable> myvar(n);
-
-  myvar = MyVar(myvar, n, var, mych, Gains, SPEAmp, Rebin);
-
+  myvar = MyVar(myvar, n, var, mych, conv_fact);
   if(CustomMaxRange!=0) for(int i=0;i<n;i++) myvar[i].limitup=CustomMaxRange;
 
-  TFile *ifile[n]; TNtuple *nt[n]; TH1D *h[n];
-  for(int i=0;i<n;i++) ifile[i]= new TFile(Form("/pnfs/ciemat.es/data/neutrinos/Super-cells_LAr/Feb22/AnalysisROOT/run%02i_NTuple.root",runs[i])); 
-  
-  Float_t ch; Float_t time; Float_t _var; 
-  
+  TFile *ifile[n];
+  for(int i=0;i<n;i++) ifile[i]= new TFile(Form("%srun%02i_NTuple.root",path.c_str(),runs[i])); 
+
+  TNtuple *nt[n];
+  TNtuple *nt_ref[n];
+  Float_t ch; 
+  Float_t time; 
+  Float_t _var; 
+
+  TH1D *h[n];
+  TH1D *aux[n];
   for(int i=0;i<n;i++)
   {
-    nt[i]= (TNtuple*)ifile[i]->Get("ntuple");
-    nt[i]->SetBranchAddress("ch",&ch);
-    nt[i]->SetBranchAddress("time",&time);
+    nt_ref[i]= (TNtuple*)ifile[i]->Get("ntuple");
+    nt[i]= (TNtuple*)ifile[i]->Get("charge");
+    // lets_pause();
+    // cout<<"hecho1"<<endl;
+    nt_ref[i]->SetBranchAddress("ch",&ch);
+    // lets_pause();
+    // cout<<"hecho2"<<endl;
+    nt_ref[i]->SetBranchAddress("time",&time);
+    // lets_pause();
+    // cout<<"hecho3"<<endl;
     nt[i]->SetBranchAddress(myvar[i].var.c_str(),&_var);
+    // lets_pause();
+    // cout<<"hecho4"<<endl;
+
     if(rate) h[i]=new TH1D(Form("h%i",i),Form("%s;%s; Events/s",name[i].c_str(),myvar[i].title.c_str()),400,myvar[i].limitdown,myvar[i].limitup);
     else h[i]=new TH1D(Form("h%i",i),Form("%s;%s; NEvents",name[i].c_str(),myvar[i].title.c_str()),400,myvar[i].limitdown,myvar[i].limitup);
-    h[i]->SetLineColor(i+1);if (i==4) h[i]->SetLineColor(i+3);//yellow sucks hard
+    h[i]->SetLineColor(i+1);if (i==4) h[i]->SetLineColor(i+3); if (i==9) h[i]->SetLineColor(30); // avoid bad colors
     h[i]->SetLineWidth(2);
+    if(rate) aux[i]=new TH1D(Form("h%i",i),Form("%s;%s; Events/s",name[i].c_str(),myvar[i].title.c_str()),400,myvar[i].limitdown,myvar[i].limitup);
+    else aux[i]=new TH1D(Form("h%i",i),Form("%s;%s; NEvents",name[i].c_str(),myvar[i].title.c_str()),400,myvar[i].limitdown,myvar[i].limitup);
+    aux[i]->SetLineColor(i+1);if (i==4) aux[i]->SetLineColor(i+3); if (i==9) aux[i]->SetLineColor(30); // avoid bad colors
+    aux[i]->SetLineWidth(2);
   }
-  
-  cout << "Drawing " << n<< endl;
 
+  cout << "Drawing " << n<< endl;
+  double max_charge;
   for(int i=0;i<n;i++)
   {
     for(int j=0;j<nt[i]->GetEntries();j++)
     { 
       nt[i]->GetEntry(j);
+      nt_ref[i]->GetEntry(j);
       if(ch!=mych[i]) continue;
       //if (runs[i]==19 && ch==1) {cout << ch << " " << _var << " " << time << " " << mych[i] << " - " << _var*myvar[i].UnitConversion << endl; lets_pause();}
       h[i]->Fill(_var*myvar[i].UnitConversion);
-      //~cout<<_var*myvar[i].UnitConversion<<endl;//debug
+      //cout<<_var*myvar[i].UnitConversion<<endl;//debug
     }
-
-    nt[i]->GetEntry(0);
+    aux[i]= (TH1D*)h[i]->Clone();
+    // cout << h[i]->GetMaximum();
+    h[i]->Rebin(Rebin);
+    if(normalize)
+    {
+      h[i]->Scale(1./ h[i]->GetMaximum());
+    }
+  
+    nt_ref[i]->GetEntry(0);
     double firstime=time;
-    nt[i]->GetEntry(nt[i]->GetEntries()-1);
+    nt_ref[i]->GetEntry(nt_ref[i]->GetEntries()-1);
     double lasttime=time; 
     Double_t duration = (lasttime - firstime)*8.e-9;
-    //~cout << duration << endl; lets_pause();
+    //cout << duration << endl; lets_pause();
     if(rate) h[i]->Scale(1./duration);
-    else ;//h[i]->Scale(1./h[i]->GetEntries());
+    // else ;//h[i]->Scale(1./h[i]->GetEntries());
   }
 
   TCanvas *c;
   if(!pad){ c  = new TCanvas("c"); c->cd();}
   else {pad->cd();}
   gStyle->SetOptTitle(0); gStyle->SetOptStat(0);
-  gPad->SetLogy();
-    
+  // gPad->SetLogy();
+
   for(int i=0;i<n;i++)
   { 
-  
-    //~gPad->SetGrid(1,1);
-  
+    //gPad->SetGrid(1,1);
     h[i]->Draw("HIST SAME"); 
-    h[i]->Rebin(4);
-    //~h[i]->Smooth();
+    // h[i]->Rebin(4);
+    //h[i]->Smooth();
 
     gPad->Update();
-    //~h[i]->GetXaxis()->SetRange(h[i]->FindBin(400),h[i]->FindBin(700));
-    //~gPad->Update();
-    //~cout<<h[i]->GetMean()<<endl; 
+    //h[i]->GetXaxis()->SetRange(h[i]->FindBin(400),h[i]->FindBin(700));
+    //gPad->Update();
+    //cout<<h[i]->GetMean()<<endl; 
   }
 
-  //~bool fit=false;
-  bool fit=false;
-
-  TF1 *f[n] ;  //gaussianas para el ajuste
-  if (fit==true)
-  {
-    //Patricia code
-    ofstream ofs("Gaus_Results.txt",std::ofstream::out | std::ofstream::app);
-    ofs <<"----------------------------------------------"<<endl;
-
-    for(int i=0;i<n;++i)
-    {
-      h[i]->GetYaxis()->SetRangeUser(1e-3,30);
-      //~double min=380,max=680;
-      double min=25,max=60;
-      //~f[i] = new TF1("f","[p0]*exp(-0.5*((x-[p1])/[p2])*((x-[p1])/[p2]))+[p3]*exp(-0.5*((x-[p4])/[p5])*((x-[p4])/[p5]))  ",min,max);//PMT Doble Gaussiana
-      f[i] = new TF1("f","gaus",min,max);
-      float par[6]={1,450,10,1,550,10};
-      for (int j=0;j<6;j++) f[i]->SetParameter(j,par[j]);
-      h[i]->Fit(f[i],"R","SAME",min,max);
-      f[0]->SetRange(myvar[i].limitdown,myvar[i].limitup);f[0]->Draw("SAME");f[i]->Print();gPad->Update();
-      //~lets_pause(); 
-      cout << runs[i] << "\t"<< mych[i]<<"\t" << f[i]->GetParameter(1) << "\t" << f[i]->GetParameter(2)<<"\t"<<var[i]<< endl;
-      
-    } 
-
-    float totalrate[n];
-    for (int j=0; j<n;j++)for (int i=-1000;i<1000;i++)totalrate[j]+=f[j]->Eval(i);
-
-    cout<<"{";	
-    //~for(int i=0;i<n-1;++i) cout<<f[i]->Integral(-1000,1000)/h[i]->GetBinWidth(1)<<",";
-    //~cout<<f[n-1]->Integral(-1000,1000)/h[n-1]->GetBinWidth(1);
-    for(int i=0;i<n-1;++i) cout<<totalrate[i]/h[i]->GetBinWidth(1)<<",";
-    cout<<totalrate[n-1]/h[n-1]->GetBinWidth(1);
-    //~cout<<f[0]->Integral(-1000,1000)/h[n-1]->GetBinWidth(1);
-    cout<<"}"<<endl;	
-  }
-
+  bool fit=true;
+  // bool fit=false;
   // draw the legend
   TLegend *legend=new TLegend(0.7, 0.6, 0.9, 0.9);
-  //~legend->SetTextFont(72);
-  //~legend->SetTextSize(0.04);
-  for(int i=0;i<n;++i)legend->AddEntry(h[i],name[i].c_str());
-  if (fit==true)legend->AddEntry(f[0],"Gauss Fit","l");
+  //legend->SetTextFont(72);
+  //legend->SetTextSize(0.04);
+  if(fit!=true) for(int i=0;i<n;++i) legend->AddEntry(h[i],name[i].c_str());
+  
+  if(fit==true)
+  {
+    lets_pause();
+    double min_fit, max_fit;
+	  cout << "Min value for fit: "; cin >> min_fit;
+	  cout << "Max value for fit: "; cin >> max_fit;
+    string filename;
+    cout << "File name?:"; cin >> filename;
+    
+    for(int i=0;i<n;i++)
+    {
+      int npeaks = 1;
+	    double_t par[100];
+	    h[i]->GetXaxis()->SetRangeUser(min_fit, max_fit);
+	    TSpectrum *s = new TSpectrum(2*npeaks,1);
+	    Int_t nfound = s->Search(h[i],2,"goff",0.01);
+	    printf("Found %d candidate peaks to fit\n",nfound);
+	    h[i]->GetXaxis()->SetRange(0,0);
+	    Double_t *xpeaks;
+	    xpeaks = s->GetPositionX();
+	
+   	  for (int p=0;p<npeaks;p++)
+      {
+        Double_t xp = xpeaks[p];
+        Int_t bin = h[i]->GetXaxis()->FindBin(xp);
+        Double_t yp = h[i]->GetBinContent(bin);
+        //if (yp-TMath::Sqrt(yp) < fline->Eval(xp)) continue;
+        par[3*p] = yp; // "height"
+        par[3*p+1] = xp; // "mean"
+        par[3*p+2] = 1; // "sigma"
+        // cout << Form("sigma_%i: ", p); cin >> par[3*npeaks+2];
+        //~ p++;
+	    }
+	   
+     //int_t max_bin[npeaks], j=0;
+	   string init = Form("gaus(0)");
+	   string added;
+	   for (int i=1; i<npeaks; i++){
+   		added = Form("+gaus(%i)",i*3);
+	   	init = init + added;
+		}
+
+    //int Nbins = h->GetNbinsX();
+    TF1 *fb = new TF1("fb",init.c_str(),min_fit,max_fit);
+    fb->SetParameters(par);
+    fb->SetLineColor(i+1); if (i==4) fb->SetLineColor(i+3); if (i==9) fb->SetLineColor(30);
+    fb->Draw("SAME");
+    legend->AddEntry(aux[i],name[i].c_str(),"l");
+    h[i]->Fit("fb","MER+","",min_fit, max_fit); gPad->Update();
+    //  if (i>0 && i<n) h[i]->Delete();
+    h[i]->SetLineWidth(0);
+    cout << h[i]->GetBinWidth(h[i]->GetMaximumBin()) << endl;
+    // TH1 *hd = (TH1*)gPad->GetPrimitive(Form("h%i",i)); gPad->GetListOfPrimitives()->Remove(hd);
+    //  lets_pause();
+    { // ------------ Volcamos a txt --------------- 
+    double mu = fb->GetParameter(3*0+1), Dmu = fb->GetParError(3*0+1);
+	  double sigma = fb->GetParameter(3*0+2), Dsigma = fb->GetParError(3*0+2);
+	  double amp = fb->GetParameter(0), Damp = fb->GetParError(0);
+    
+	  // FILE *f = fopen(Form("prueba.txt"), "a");
+	  FILE *f = fopen(Form("fit_data/%s.txt",filename.c_str()), "a");
+	  if (f == NULL)
+	  {
+		printf("Error opening file!\n");
+		exit(1);
+	  }
+	  fprintf(f, "%f\t%f\t%f\t%f\t%f\t%f\n", mu, Dmu, sigma, Dsigma, amp, Damp);
+	  fclose(f);
+	  } //*/
+	  //  lets_pause();
+    }
+  }
+
   legend->Draw();
-  gStyle->SetOptFit();
+  //  gStyle->SetOptFit();
    
-  TPaveText *t = new TPaveText(0.3, 0.85, 0.7, 0.95, "brNDC"); // middle
-  t->AddText("SiPM Charge Spectrum - Triggers");
-  t->Draw();
-  lets_pause();
+TPaveText *t = new TPaveText(0.3, 0.85, 0.7, 0.95, "brNDC"); // middle
+t->AddText("SiPM #290 - OV = 4.0 V");
+// t->AddText("SuperCell Charge Spectrum - OV = 7.0 V");
+// t->AddText("SuperCell PE - OV = 3.5");
+// t->AddText("SuperCell PE/mm^2 - OV = 3.5");
+// t->AddText("PMT Charge (1400V)");
+t->Draw();
+lets_pause();
+lets_pause();
 }
 
-void ChargeSpectrum()
+void ChargeSpectrum(string input = "cs_config_file.txt")
 {
-  Draw({0},{14},{"QTotal"},false,{"QTotal"});
+  int ch;
+  ch = IntInput(input, "CHANNEL");
+
+  string path; string range_type;
+  path = StringInput(input, "PATH"); range_type = StringInput(input, "RANGE_TYPE");
+
+  bool debug;
+  debug = BoolInput(input, "DEBUG");
+
+  /* >> Empleamos el json para seleccionar las runes */
+  const Run_map map_of_runs = Run_map("mapa_feb_2.json");
+  std::vector<int> runs;
+  std::vector<string> label;
+  std::vector<double> gains_sipm289;
+  std::vector<double> gains_sipm290;
+  std::vector<double> gains_pmt;
+  std::vector<double> gains_sc;
+  std::vector<double> conv_fact;
+
+  for (auto i:map_of_runs.json_map) 
+  {
+    if (i["tipo"] == "coincidencia" && i["threshold"] == 200 &&  i["ov_sipm"] == 4 &&  i["ov_pmt"] == 1200)
+    {
+      runs.push_back(i["run"]);
+      label.push_back(to_string(i["dia"])+" Feb");
+    }
+    if (i["run"] == 1)
+    {
+      conv_fact.push_back(i["factor_conv_sipm"]);
+      conv_fact.push_back(i["factor_conv_pmt"]);
+      conv_fact.push_back(i["factor_conv_sc"]);
+    }
+  }
+
+  int n = runs.size();
+  std::vector<int> channels(n,ch);
+
+  vector<string> var(n,range_type);
+
+  /* >> Ganancias si no estan leidas del json */
+  std::vector<double> gains_sipm1(n,6.77E+06); //Mean + 4OV + ped-1PE
+
+
+  if (debug)
+  {
+    for (auto w:channels) cout << "Channels: " << w << endl;
+    for (auto test:runs) cout << "Runs: " << test << endl;
+    for (auto l:label) cout << l << endl;
+    for (auto i:var) cout << "Charge Range: " << i << endl;
+    for (auto test5:conv_fact) cout << test5 << endl;
+  }
+
+  /* En esta funcion las ganancias de todos los canales estan definidos como vectores */
+  Draw_vector(path, channels, runs, label, true, var, conv_fact, 236, 415.47, 415.47, false, 1);
 }
--- lib/MyVar.h
+++ lib/MyVar.h
@@ -1,46 +1,68 @@
 struct variable {string var; string title; double limitdown; double limitup; double UnitConversion;};
 
-std::vector<variable> MyVar(std::vector<variable> myvar, int n, std::vector<string> var, std::vector<int> mych, std::vector<double> Gains, std::vector<double> SPEAmp, double Rebin)
+std::vector<variable> MyVar(std::vector<variable> myvar, int n, std::vector<string> var, std::vector<int> mych, std::vector<double> conv_fact)
 {   
     for(int i=0;i<n;i++)
     {
-        if (var[i]=="TStartQpeak" ) myvar[i]={"TStartQpeak","T (s)",1e-6,3e-6,1.0};
-        if (var[i]=="TEndQPeak" ) myvar[i]={"TEndQPeak","Time (s)",1e-6,3e-6,1.0};
-        if (var[i]=="TDiff" ) myvar[i]={"TDiff","Time (s)",0e-6,2e-6,1.0};
-        
-        if (var[i]=="Q1" && mych[i]==0) myvar[i]={"Q1","Charge collected (pC)",0,3000,1.0};
-        if (var[i]=="Q1" && mych[i]!=0) myvar[i]={"Q1","Charge collected (pC)",0,500,1.0};
-        
-        if (var[i]=="Q2" && mych[i]==0) myvar[i]={"Q2","Charge collected (pC)",0,3000,1.0};
-        if (var[i]=="Q2" && mych[i]!=0) myvar[i]={"Q2","Charge collected (pC)",0,500,1.0};
-        
-        if (var[i]=="Q3" && mych[i]==0) myvar[i]={"Q3","Charge collected (pC)",0,3000,1.0};
-        if (var[i]=="Q3" && mych[i]!=0) myvar[i]={"Q3","Charge collected (pC)",0,500,1.0};
-        
-        if (var[i]=="QTotal" && mych[i]==0) myvar[i]={"QTotal","Charge collected (pC)",0,3000,1.0};
-        if (var[i]=="QTotal" && mych[i]!=0) myvar[i]={"QTotal","Charge collected (pC)",0,500,1.0};
-        
-        
-        if (var[i]=="Amp" && mych[i]==0) myvar[i]={"Amp","Amplitude (ADC)",0,2000,1.0};
-        if (var[i]=="Amp" && mych[i]!=0) myvar[i]={"Amp","Amplitude (ADC)",0,2000,1.0};
+        if (var[i]=="TStartQpeak") myvar[i]={"TStartQpeak","T (s)",1e-6,3e-6,1.0};
+        if (var[i]=="TEndQPeak") myvar[i]={"TEndQPeak","Time (s)",1e-6,3e-6,1.0};
+        if (var[i]=="TDiff") myvar[i]={"TDiff","Time (s)",0e-6,2e-6,1.0};
 
-        if (var[i]=="Q1PE"&& mych[i]!=0) myvar[i]={"Q1","Charge collected (PE)",0,130,1.0/Gains[mych[i]]};
-        if (var[i]=="Q1PE"&& mych[i]==0) myvar[i]={"Q1","Charge collected (PE)",0,1000,1.0/Gains[mych[i]]};
+        if (var[i]=="Q1" && (mych[i]==0 || mych[i]==1)) myvar[i]={"Q1","Charge collected (pC)",0,100,1.0/conv_fact[0]}; //SiPM
+        if (var[i]=="Q1" && mych[i]==2) myvar[i]={"Q1","Charge collected (pC)",0,5000,1.0/conv_fact[1]}; // PMT
+        if (var[i]=="Q1" && mych[i]==3) myvar[i]={"Q1","Charge collected (pC)",0,200,1.0/conv_fact[2]}; //SC
+
+        if (var[i]=="Q2" && (mych[i]==0 || mych[i]==1)) myvar[i]={"Q2","Charge collected (pC)",0,100,1.0/conv_fact[0]}; //SiPM
+        if (var[i]=="Q2" && mych[i]==2) myvar[i]={"Q2","Charge collected (pC)",0,5000,1.0/conv_fact[1]}; // PMT
+        if (var[i]=="Q2" && mych[i]==3) myvar[i]={"Q2","Charge collected (pC)",0,200,1.0/conv_fact[2]}; //SC
+
+        if (var[i]=="Q3" && (mych[i]==0 || mych[i]==1)) myvar[i]={"Q3","Charge collected (pC)",0,100,1.0/conv_fact[0]}; //SiPM
+        if (var[i]=="Q3" && mych[i]==2) myvar[i]={"Q3","Charge collected (pC)",0,5000,1.0/conv_fact[1]}; // PMT
+        if (var[i]=="Q3" && mych[i]==3) myvar[i]={"Q3","Charge collected (pC)",0,200,1.0/conv_fact[2]}; //SC
         
-        if (var[i]=="Q2PE"&& mych[i]!=0) myvar[i]={"Q2","Charge collected (PE)",0,130,1.0/Gains[mych[i]]};
-        if (var[i]=="Q2PE"&& mych[i]==0) myvar[i]={"Q2","Charge collected (PE)",0,1000,1.0/Gains[mych[i]]};
+        if (var[i]=="QTotal" && (mych[i]==0 || mych[i]==1)) myvar[i]={"QTotal","Charge collected (pC)",0,100,1.0/conv_fact[0]}; //SiPM
+        if (var[i]=="QTotal" && mych[i]==2) myvar[i]={"QTotal","Charge collected (pC)",0,5000,1.0/conv_fact[1]}; // PMT
+        if (var[i]=="QTotal" && mych[i]==3) myvar[i]={"QTotal","Charge collected (pC)",0,100,1.0/conv_fact[2]}; //SC
         
-        if (var[i]=="Q3PE"&& mych[i]!=0) myvar[i]={"Q3","Charge collected (PE)",0,130,1.0/Gains[mych[i]]};
-        if (var[i]=="Q3PE"&& mych[i]==0) myvar[i]={"Q3","Charge collected (PE)",0,1000,1.0/Gains[mych[i]]};
+        if (var[i]=="Amp" && (mych[i]==0 || mych[i]==1)) myvar[i]={"Amp","Amplitude (ADC)",0,1500,1.0}; //SiPM
+        if (var[i]=="Amp" && mych[i]==2) myvar[i]={"Amp","Amplitude (ADC)",0,15000,1.0}; // PMT
+        if (var[i]=="Amp" && mych[i]==3) myvar[i]={"Amp","Amplitude (ADC)",0,12000,1.0}; //SC
 
-        if (var[i]=="QPeak"&& mych[i]==0) myvar[i]={"QPeak","Charge Peak",0,2000,1.0};
-        if (var[i]=="QPeak"&& mych[i]!=0) myvar[i]={"QPeak","Charge Peak",0,500,1.0};
+        if (var[i]=="QPeak" && (mych[i]==0 || mych[i]==1)) myvar[i]={"QPeak","Charge Peak",0,200,1.0/(conv_fact[0])}; //SiPM
+        if (var[i]=="QPeak" && mych[i]==2) myvar[i]={"QPeak","Charge Peak",0,200,1.0/(conv_fact[1])}; // PMT
+        if (var[i]=="QPeak" && mych[i]==3) myvar[i]={"QPeak","Charge Peak",0,200,1.0/(conv_fact[2])}; //SC
 
-        if (var[i]=="QPeakPE"&& mych[i]==0) myvar[i]={"QPeakPE","Charge Peak (PE)",0,6000*Rebin,1.0/Gains[mych[i]]};
-        if (var[i]=="QPeakPE"&& mych[i]!=0) myvar[i]={"QPeakPE","Charge Peak (PE)",0,110*Rebin,1.0/Gains[mych[i]]};
+        if (var[i]=="QFixRange1" && (mych[i]==0 || mych[i]==1)) myvar[i]={"QFixRange1","Charge collected (pC)",0,120,1.0/(conv_fact[0])}; //SiPM
+        if (var[i]=="QFixRange1" && (mych[i]==2)) myvar[i]={"QFixRange1","Charge collected (pC)",0,8000,1.0/(conv_fact[1])}; //PMT
+        if (var[i]=="QFixRange1" && (mych[i]==3)) myvar[i]={"QFixRange1","Charge collected (pC)",0,300,1.0/(conv_fact[2])}; //SuperCell
 
-        if (var[i]=="AmpPE"&& mych[i]!=0) myvar[i]={"Amp","Amplitude (PE)",0,200,1.0/SPEAmp[mych[i]]};
-        if (var[i]=="AmpPE"&& mych[i]==0) myvar[i]={"Amp","Amplitude (PE)",0,1000,1.0/SPEAmp[mych[i]]};
+        if (var[i]=="QFixRange2" && (mych[i]==0 || mych[i]==1)) myvar[i]={"QFixRange2","Charge collected (pC)",0,120,1.0/(conv_fact[0])}; //SiPM
+        if (var[i]=="QFixRange2" && (mych[i]==2)) myvar[i]={"QFixRange2","Charge collected (pC)",0,8000,1.0/(conv_fact[1])}; //PMT
+        if (var[i]=="QFixRange2" && (mych[i]==3)) myvar[i]={"QFixRange2","Charge collected (pC)",0,300,1.0/(conv_fact[2])}; //SuperCell
+
+        if (var[i]=="QFixRange3" && (mych[i]==0 || mych[i]==1)) myvar[i]={"QFixRange3","Charge collected (pC)",0,120,1.0/(conv_fact[0])}; //SiPM
+        if (var[i]=="QFixRange3" && (mych[i]==2)) myvar[i]={"QFixRange3","Charge collected (pC)",0,8000,1.0/(conv_fact[1])}; //PMT
+        if (var[i]=="QFixRange3" && (mych[i]==3)) myvar[i]={"QFixRange3","Charge collected (pC)",0,300,1.0/(conv_fact[2])}; //SuperCell
+
+        if (var[i]=="QFixRange4" && (mych[i]==0 || mych[i]==1)) myvar[i]={"QFixRange4","Charge collected (pC)",0,120,1.0/(conv_fact[0])}; //SiPM
+        if (var[i]=="QFixRange4" && (mych[i]==2)) myvar[i]={"QFixRange4","Charge collected (pC)",0,8000,1.0/(conv_fact[1])}; //PMT
+        if (var[i]=="QFixRange4" && (mych[i]==3)) myvar[i]={"QFixRange4","Charge collected (pC)",0,300,1.0/(conv_fact[2])}; //SuperCell
+
+        if (var[i]=="QPeakRange1" && (mych[i]==0 || mych[i]==1)) myvar[i]={"QPeakRange1","Charge collected (pC)",0,400,1.0/(conv_fact[0])}; //SiPM
+        if (var[i]=="QPeakRange1" && (mych[i]==2)) myvar[i]={"QPeakRange1","Charge collected (pC)",0,5000,1.0/(conv_fact[1])}; //PMT
+        if (var[i]=="QPeakRange1" && (mych[i]==3)) myvar[i]={"QPeakRange1","Charge collected (pC)",0,300,1.0/(conv_fact[2])}; //SuperCell
+
+        if (var[i]=="QPeakRange2" && (mych[i]==0 || mych[i]==1)) myvar[i]={"QPeakRange2","Charge collected (pC)",0,400,1.0/(conv_fact[0])}; //SiPM
+        if (var[i]=="QPeakRange2" && (mych[i]==2)) myvar[i]={"QPeakRange2","Charge collected (pC)",0,5000,1.0/(conv_fact[1])}; //PMT
+        if (var[i]=="QPeakRange2" && (mych[i]==3)) myvar[i]={"QPeakRange2","Charge collected (pC)",0,300,1.0/(conv_fact[2])}; //SuperCell
+
+        if (var[i]=="QPeakRange3" && (mych[i]==0 || mych[i]==1)) myvar[i]={"QPeakRange3","Charge collected (pC)",0,400,1.0/(conv_fact[0])}; //SiPM
+        if (var[i]=="QPeakRange3" && (mych[i]==2)) myvar[i]={"QPeakRange3","Charge collected (pC)",0,5000,1.0/(conv_fact[1])}; //PMT
+        if (var[i]=="QPeakRange3" && (mych[i]==3)) myvar[i]={"QPeakRange3","Charge collected (pC)",0,300,1.0/(conv_fact[2])}; //SuperCell
+
+        if (var[i]=="QPeakRange4" && (mych[i]==0 || mych[i]==1)) myvar[i]={"QPeakRange4","Charge collected (pC)",0,400,1.0/(conv_fact[0])}; //SiPM
+        if (var[i]=="QPeakRange4" && (mych[i]==2)) myvar[i]={"QPeakRange4","Charge collected (pC)",0,5000,1.0/(conv_fact[1])}; //PMT
+        if (var[i]=="QPeakRange4" && (mych[i]==3)) myvar[i]={"QPeakRange4","Charge collected (pC)",0,300,1.0/(conv_fact[2])}; //SuperCell
     }
     return myvar;
 }    
